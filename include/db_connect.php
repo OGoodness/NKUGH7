2517d3c7f54e2e54d037234c24d3b90478e8a60f
Unknown <budhathokg1@nku.edu>
2018-10-13 00:45:36 -0400

    created sql folder and database folder

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,9 +1,9 @@
 <?php
 ini_set('display_errors', 'On');
-$servername = "mysql-instance-gb1.chajedmv8mjh.us-east-2.rds.amazonaws.com";
+$servername = "globalhack7.ced2x9toup5e.us-east-2.rds.amazonaws.com";
 $username = "norse";
 $password = "norsehacks";
-$database = "norse";
+$database = "globalhack7";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
---
+++ sql/globalhack7.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 13, 2018 at 12:43 AM
+-- Server version: 5.7.23-0ubuntu0.18.04.1
+-- PHP Version: 7.2.10-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `globalhack7`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(10) NOT NULL,
+  `user_type` varchar(256) NOT NULL,
+  `user_email` varchar(256) NOT NULL,
+  `user_passwd` varchar(256) NOT NULL,
+  `user_descritption` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
