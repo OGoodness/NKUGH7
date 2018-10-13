5e8a822d95df2cefab7f29be63b386c28d73779a
Unknown <budhathokg1@nku.edu>
2018-10-13 11:30:09 -0400

    fixed issues with signup

--- signup.php
+++ signup.php
@@ -171,10 +171,13 @@
 <main>
         <div class="content" style="margin: 0 auto;">
             <div class="text"><?php echo $content["login__sign_in_account"]; ?></div>
-            <form action="mainPage.php" method="post">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                 <input id ="" type="text" name="fname" placeholder="first name"><br>
+                <br><span class="help-block"><?php echo $userFirstNameErr; ?></span>
                 <input id ="" type="text" name="lname" placeholder="last name"><br>
-                <input id ="" type="text" name="email" placeholder="johnsmith@email.com"><br>
+                <br><span class="help-block"><?php echo $userLastNameErr; ?></span>
+                <input id ="" type="text" name="email" placeholder="johnsmith@email.com">
+                <br><span class="help-block"><?php echo $emailErr; ?></span><br>
                 <select name="user-type">
                     <?php
                         foreach($user_type as $data){
@@ -184,11 +187,14 @@
                         }
                     ?>
                 </select><br>
-                <input id="" type="password" name="lname" placeholder="password"><br>
-                <input id="" type="password" name="lname" placeholder="confirm_password"><br>
+                <input id="" type="password" name="password" placeholder="password"><br>
+                <br><span class="help-block"><?php echo $passwordErr; ?></span>
+                <input id="" type="password" name="confirm_password" placeholder="confirm_password"><br>
+                <br><span class="help-block"><?php echo $confirmPasswordErr; ?></span>
+                <button type="submit"><?php echo $content["login__sign_up_button"]; ?></button>
             </form>
 
-            <button type="submit"><?php echo $content["login__sign_up_button"]; ?></button>
+            
         </div>
     </div>
 </div>
