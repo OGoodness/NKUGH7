8ec9cc01c2d606699dfa1345b4d9e628bc22a7eb
Tobel <tobelwubshet@gmail.com>
2018-10-13 06:15:40 -0400

    added connection to database

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,15 +1,17 @@
 <?php
 ini_set('display_errors', 'On');
-$servername = "globalhack7.ced2x9toup5e.us-east-2.rds.amazonaws.com";
-$username = "norse";
+$servername = "ec2-18-188-172-221.us-east-2.compute.amazonaws.com";
+$username = "ec2-user";
 $password = "norsehacks";
 $database = "globalhack7";
-
+echo "Connection started";
 // Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
+$dbc = mysqli_connect ($servername, $username, $password, $database) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
+//$conn = new mysqli($servername, $username, $password, $database);
+echo "connection finished";
 // Check connection
-if ($conn->connect_error) {
+/*if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
  ?>
+*/
