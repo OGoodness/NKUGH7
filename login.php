62bdd285b7761dbad1809b4ab0e79e9b0e2d48cc
Griffin Burkhardt <burkhardtg1@nku.edu>
2018-10-13 04:18:23 -0400

    Began login page

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <title>Griffin Burkhardt | Portfolio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/theme.css">
+    <script src="js/main.js"></script>
+    </head>
+
+<body class="center">
+
+<header>
+    <div class="title"><b>Returning user</b>, Select your language</div>
+    <ul class="languages">
+        <li>elige tu idioma</li> <!--Spanish-->
+        <li>اختر لغتك</li> <!--Arabic-->
+        <li>chọn ngôn ngữ của bạn</li> <!--Vietnamese-->
+        <li>Piliin ang iyong wika</li> <!--Filipino-->
+    </ul>
+</header>
+
+<main>
+    <div class="two-column-container">
+        <img id="flag-filipino" src="images/flag-filipino.png" alt="Filipino Flag" data="Filipino" onclick="languageSelect('flag-filipino')">
+        <img id="flag-usa" src="images/flag-usa.png" alt="USA Flag" data="English" onclick="languageSelect('flag-usa')">
+        <img id="flag-arabic" src="images/flag-arabic.png" alt="Arabic Flag" data="Arabic" onclick="languageSelect('flag-arabic')">
+        <img id="flag-vietnam" src="images/flag-vietnam.png" alt="Vietnam Flag" data="Vietnamese" onclick="languageSelect('flag-vietnam')">
+        <img id="flag-spanish" src="images/flag-spanish.png" alt="Spanish Flag" data="Spanish" onclick="languageSelect('flag-spanish')">
+    </div>
+</main>
+
+</body>
+</html>
