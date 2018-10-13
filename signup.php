c253d6384d3b1e32e27a5ea4d84929c6d1e805b6
Unknown <budhathokg1@nku.edu>
2018-10-13 15:58:36 -0400

    push for translate signup

--- signup.php
+++ signup.php
@@ -5,23 +5,7 @@
     require_once ('vendor/autoload.php');
     use \Statickidz\GoogleTranslate;
 
-    $content["error_first_name"] = 'Please enter your first name.';
-    $content["error_last_name"] = 'Please enter your last name.';
-    $content["error_email"] = 'Please enter an email.';
-    $content["error_empty_password"] = 'Please enter a password.';
-    $content["error_length_password"] = 'Password must have at least 6 characters.';
-    $content["error_confirm_password"] = 'Please confirm password.';
-    $content["error__password_ dont_match"] = 'Password did not match.';
-
-    $content["error__prepared_statement"] = 'Something went wrong. Please try again later.';
-    
-
-
-    $trans = new GoogleTranslate();
-    foreach($content as $key => $text){
-        $content["$key"] = $trans->translate("en", $language, $text);
-    }
-
+ 
     // Define variables and initialize with empty values
     $userFirstName = $userLastName = $type = $password = $confirmPassword = $email = "";
     $userFirstNameErr = $userLastNameErr = $userTypeErr = $passwordErr = $confirmPasswordErr = $emailErr = "";
