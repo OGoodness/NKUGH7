41269f7ac8152287cbb948ceada9d180efb7c543
Weston Rainer <Rainerw1@mymail.nku.edu>
2018-10-13 13:47:11 -0500

    Update map.php

--- map.php
+++ map.php
@@ -4,27 +4,8 @@
     $hostLongitude = -84.401139; //union ky
     $migrantLatitude = 39.14300;
     $migrantLongitude = -84.203060;//montgomery
-    $centerLatitude = 0;
-    $centerLongitude = 0;
-
-    if($hostLatitude > $migrantLatitude)
-    {
-        $centerLatitude = $hostLatitude - ($hostLatitude - $migrantLatitude);
-    }
-    else
-    {
-        $centerLatitude = $migrantLatitude + ($hostLatitude - $migrantLatitude);
-    }
-
-    if($hostLongitude > $migrantLongitude)
-    {
-        $centerLongitude = $hostLongitude - ($hostLongitude - $migrantLongitude);
-    }
-    else
-    {
-        $centerLongitude = $migrantLongitude + ($hostLongitude- $migrantLongitude);
-    }
-
+    $centerLatitude = ($hostLatitude + $migrantLatitude) /2;
+    $centerLongitude = $hostLongitude - ($hostLongitude - $migrantLongitude);
 ?>
 <html>
     <head>
@@ -35,33 +16,32 @@
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         
-        
    
     </head>
-    <div class = "map-container">
+ <!--<div class = "map-container">
         <div class="row">
-            <div class = "col-md-6">
-                <div class="google-map">
+            <div class = "col-md-6">-->
+            <div id="googleMap" style="width:80%;height:400px;"></div>
                     <script>
                         function initMap()
                         {
-                            var hostLocation = {lat: "<?php echo $hostLatitude; ?>",lng: "<?php echo $hostLongitude; ?>"};
-                            var migrantLocation =  {lat: "<?php echo $migrantLatitude; ?>",lng: "<?php echo $migrantLongitude; ?>"};
-                            var centerLocation = {lat: "<?php echo $centerLatitude; ?>",lng: "<?php echo $centerLongitude; ?>"};
-                            var map = new gooogle.maps.Map(document.getElementsByClassName("google-map"), {zoom: 4, center: centerLocation});
+                            var hostLocation = {lat: <?php echo $hostLatitude; ?>,lng: <?php echo $hostLongitude; ?>};
+                            var migrantLocation =  {lat: <?php echo $migrantLatitude; ?>,lng: <?php echo $migrantLongitude; ?>};
+                            var centerLocation = {lat: <?php echo $centerLatitude; ?>,lng: <?php echo $centerLongitude; ?>};
+                            var map = new google.maps.Map(document.getElementById("googleMap"), {zoom: 4, center: centerLocation});
                             var markerCenter = new google.maps.Marker({position: centerLocation, map: map});
-                            var markerHost = new google.maps.Marker({position: centerLocation, map: map});
+                            var markerHost = new google.maps.Marker({position: hostLocation, map: map});
                             var markerMigrant = new google.maps.Marker({position: migrantLocation, map: map});
                         }
                     </script>
-                    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AlzaSyAhc-gJsL4DOC6hA5DUW-uZGLCR_vPDkSk&call"></script>
-                </div>
-            </div>
+                    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhc-gJsL4DOC6hA4DUW-uZGLCR_vPDkSk&callback=initMap"></script>
+                
+           <!-- </div>
             <div class="col-md-6">
                 <div class="info">
                     
                 </div>
             </div>
         </div>
-    </div>
+    </div>-->
 </html>
