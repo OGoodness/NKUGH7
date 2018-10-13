78c04b1f4e9d0b0d32759110b0290eb148734f72
Unknown <budhathokg1@nku.edu>
2018-10-13 12:05:01 -0400

    signup trial 10

--- functions/functions.php
+++ functions/functions.php
@@ -1,16 +1,16 @@
 <?php
-    function getUserType($conn){
-        $response = NULL;
-        $sql = "SELECT *
-                FROM user_type";
-        if($result = $conn->query($sql)){
-            if($result->num_rows > 0){
-                while($row = $result->fetch_object()){
-                    $response[] = $row;
-                }
+//all get functions...........
+function get_user_type($conn){
+    $response = NULL;
+    $sql = "SELECT *
+    FROM globalhack7.user_type";
+    if($result = $conn->query($sql)){
+        if($result->num_rows > 0){
+            while($row = $result->fetch_object()){
+                $response[] = $row;
             }
-        $result->free();
         }
-       
-        return $response;
     }
+    $result->free();
+    return (object)$response;
+}
