1e389f96ba2368819ac85cde5428e5a2a10343e0
Weston Rainer <Rainerw1@mymail.nku.edu>
2018-10-13 08:36:12 -0500

    Revert "Merge branch 'master' of https://github.com/OGoodness/NKUGH7"
    This reverts commit 49321b1dfcbabcfbda6e9e5d9920c94c26d1a36c, reversing
    changes made to 5788522172ee3c960934533c02c4307e3c1dd7df.

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php
-if(isset($_COOKIE["language"])) {
-   // header("Location: login.php");  Comment this back in when we are ready to change language whenever
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -29,15 +23,11 @@ if(isset($_COOKIE["language"])) {
 
 <main style="margin-top: 0%;">
     <div class="container">
-        <img id="flag-filipino" src="images/flag-filipino.png" alt="Filipino Flag" data="tl" onclick="languageSelect('flag-filipino')">
+        <img id="flag-spanish" src="images/flag-spanish.png" alt="Spanish Flag" data="es" onclick="languageSelect('flag-spanish')">   
         <img id="flag-usa" src="images/flag-usa.png" alt="USA Flag" data="en" onclick="languageSelect('flag-usa')">
         <img id="flag-arabic" src="images/flag-arabic.png" alt="Arabic Flag" data="ar" onclick="languageSelect('flag-arabic')">
         <img id="flag-vietnam" src="images/flag-vietnam.png" alt="Vietnam Flag" data="vi" onclick="languageSelect('flag-vietnam')">
-<<<<<<< HEAD
         <img id="flag-filipino" src="images/flag-filipino.png" alt="Filipino Flag" data="tl" onclick="languageSelect('flag-filipino')">
-=======
-        <img id="flag-spanish" src="images/flag-spanish.png" alt="Spanish Flag" data="es" onclick="languageSelect('flag-spanish')">
->>>>>>> 6420602c8c447468b1fe30d52c081b60063c7cb2
         <img id="flag-india" src="images/flag-india.png" alt="Indian Flag" data="hi" onclick="languageSelect('flag-spanish')">
         <img id="flag-ethiopia" src="images/flag-ethiopia.png" alt="Ethiopia Flag" data="am" onclick="languageSelect('flag-ethiopia')">
         <img id="flag-bulgaria" src="images/flag-bulgaria.png" alt="Bulgaria Flag" data="bg" onclick="languageSelect('flag-ethiopia')">
