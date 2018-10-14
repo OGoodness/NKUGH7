aed3fe504876f55d86737e59745778591b4bad09
Tobel <tobelwubshet@gmail.com>
2018-10-13 23:33:16 -0400

    auto fill first last name

--- migrantForm.php
+++ migrantForm.php
@@ -172,8 +172,8 @@
             <div class="input-picture"><img src=""></div>
         </div>
         <div class="card-row-header">
-            <div class="card-field"><input required type="text" name="fname" placeholder="<?php echo $content["migrant_account__first_name_text"]; ?>"><br><?php echo strtolower($content["migrant_account__first_name_text"]); ?><span class="red">*</span></input></div>
-            <div class="card-field"><input required type="text" name="lname" placeholder="<?php echo  $content["migrant_account__last_name_text"]; ?>"><br><?php echo strtolower($content["migrant_account__last_name_text"]); ?><span class="red">*</span></input></div>
+            <div class="card-field"><input required type="text" name="fname" value="<?php echo $firstname; ?>"><br><?php echo strtolower($content["migrant_account__first_name_text"]); ?><span class="red">*</span></input></div>
+            <div class="card-field"><input required type="text" name="lname" value="<?php echo  $lastname; ?>"><br><?php echo strtolower($content["migrant_account__last_name_text"]); ?><span class="red">*</span></input></div>
             <div class="card-field"><input type="text" name="gender" placeholder="<?php echo $content["migrant_account__gender_text"]; ?>"><br><?php echo strtolower($content["migrant_account__gender_text"]); ?></input></div>
         </div>
         <div class="card-row-header">
