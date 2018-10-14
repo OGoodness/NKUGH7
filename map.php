739fb14b783f5e0fae4027ada7a47a946846a5a7
Weston Rainer <Rainerw1@mymail.nku.edu>
2018-10-14 00:49:39 -0500

    Update map.php
    it works bros

--- map.php
+++ map.php
@@ -6,6 +6,13 @@
         $migrantLongitude = -84.203060;//montgomery
         $centerLatitude = ($hostLatitude + $migrantLatitude) /2;
         $centerLongitude = ($hostLongitude + $migrantLongitude)/ 2;
+        $hostLike1 = "climbing";
+        $hostLike2 = "soccer";
+        $migrantLike1 = "movies";
+        $migrantLike2 = "concert";
+        $migrantEthnicity = "Italian";
+        $likesHostArray = array($hostLike1, $hostLike2, $migrantLike1, $migrantLike2, $migrantEthnicity);
+        $ideasArray = array();
     ?>
 <!DOCTYPE html>
 <html>
@@ -20,52 +27,75 @@
 	<title></title>
 </head>
 <body>
+<?php 
+      foreach($likesHostArray as $like){
+        $geocode = file_get_contents('https://maps.googleapis.com/maps/api/place/findplacefromtext/xml?input='.$like. '&inputtype=textquery&fields=formatted_address,name&locationbias=circle:30@'.$centerLatitude.',' . $centerLongitude.'&key=AIzaSyAhc-gJsL4DOC6hA4DUW-uZGLCR_vPDkSk');
+        #print_r($geocode);
+        $test = (array)simplexml_load_string($geocode);
+        $test = (array)$test['candidates'];
+        $ideasArray[] =  $test;
+        #print_r($ideasArray);
+         
+      }
+     # echo "<pre>";
+      #print_r($geocode);
+      #echo "</pre>";
+      #json_decode($geocode)->candidates[0];?>
 	<div class="container map-container">
 		<div class="row">
 			<div class="col-md-6" id="googleMap" style="height: 400px;"></div>
 			<script>
+        var geocoder;
                  function initMap()
                  {
                      var hostLocation = {lat: <?php echo $hostLatitude; ?>,lng: <?php echo $hostLongitude; ?>};
                      var migrantLocation =  {lat: <?php echo $migrantLatitude; ?>,lng: <?php echo $migrantLongitude; ?>};
                      var centerLocation = {lat: <?php echo $centerLatitude; ?>,lng: <?php echo $centerLongitude; ?>};
                      var map = new google.maps.Map(document.getElementById("googleMap"), {zoom: 4, center: centerLocation});
-                     var markerCenter = new google.maps.Marker({position: centerLocation, map: map});
+                     var likeMarkers = [-2000];
+                     geocoder = new google.maps.Geocoder();
+                     var array = JSON.parse('<?php echo json_encode($ideasArray); ?>')
+                     //console.log(array);
+                     for(var i = 0; i < <?php echo sizeOf($ideasArray) ?>; i++)
+                     {
+                        var address = array[i]['formatted_address'];
+                        geocoder.geocode({'address' : address}, function(results, status){
+                            likeMarkers.push(new google.maps.Marker({position: results[0].geometry.location, map: map}));
+                        
+                        });
+                        
+                     }
+                     //var markerCenter = new google.maps.Marker({position: centerLocation, map: map});
                      var markerHost = new google.maps.Marker({position: hostLocation, map: map});
                      var markerMigrant = new google.maps.Marker({position: migrantLocation, map: map});
                      
                      
                      //place predictions
                      
-                      var activity = 'amc';
-                    var location = 'Ohio';
-        var displaySuggestions = function(predictions, status) {
-          if (status != google.maps.places.PlacesServiceStatus.OK) {
-            alert(status);
-            return;
+                     
+       
+
+
+             for(var i = 0; i < <?php echo sizeof($ideasArray)?>; i++) {
+                var divcard= document.createElement('div');
+                divcard.setAttribute("class", "card");
+                var divcardbody = document.createElement('div');
+                divcardbody.setAttribute("class","card-body");
+                divcardbody.appendChild(document.createTextNode(array[i]['name'] + " " + array[i]['formatted_address']));
+                divcard.appendChild(divcardbody);
+                document.getElementById("results").appendChild(divcard);
           }
 
-          predictions.forEach(function(prediction) {
-            var divcard= document.createElement('div');
-            divcard.setAttribute("class", "card");
-              var divcardbody = document.createElement('div');
-              divcardbody.setAttribute("class","card-body");
-              divcardbody.appendChild(document.createTextNode(prediction.description));
-            divcard.appendChild(divcardbody);
-              document.getElementById("results").appendChild(divcard);
-          });
-        };
-
-        var service = new google.maps.places.AutocompleteService();
-        service.getPlacePredictions({ input: activity+' near '+location }, displaySuggestions);
+          var service = new google.maps.places.AutocompleteService();
+        
       
                  }
 			</script> 
 			 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhc-gJsL4DOC6hA4DUW-uZGLCR_vPDkSk&libraries=places&callback=initMap"
         async defer></script>
-
-			<div class="col-md-6 info" id = "results">
-				
+          
+          
+			<div class= "col-md-6 info" id = "results">
 			</div>
 		</div>
 	</div>
