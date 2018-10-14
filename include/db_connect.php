df74ae75716f8452ef7ee97819a2549704bf1bf5
Griffin Burkhardt <griffinburkhardt@gmail.com>
2018-10-14 02:00:08 -0400

    X

--- include/db_connect.php
+++ include/db_connect.php
@@ -5,7 +5,7 @@ $username = "root";
 $password = "norsehacks";
 $database = "globalhack7";
 // Create connection
-$conn = new mysqli($servername, $username, '', $database);
+$conn = new mysqli($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
