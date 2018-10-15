ae32e2c487fa09fe4ec925b9a8d7f4ea9af6b4a6
Unknown <budhathokg1@nku.edu>
2018-10-15 17:02:31 -0400

    Auto stash before rebase of "newtest"

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,11 +1,11 @@
 <?php
 ini_set('display_errors', 'On');
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "ganesh";
+$password = "Gbwalter88$";
 $database = "globalhack7";
 // Create connection
-$conn = new mysqli($servername, $username, "", $database);
+$conn = new mysqli($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
